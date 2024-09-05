c89dfd660e8dcf5813ae4beeaef4691aa39f861f
Adriano Sanderson <adrianoramos06@gmail.com>
2024-09-05 13:23:15 -0300

    Add Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,74 @@ import * as faceapi from 'face-api.js'
 function App() {
 
   const videoRef = useRef(null)
+  const canvasRef = useRef(null)
 
   console.log(videoRef.current)
 
-  useEffect(() =>{
+  useEffect(() => {
 
-    navigator.mediaDevices.getUserMedia({video: true}).then((stream) =>{
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
       const videoEl = videoRef.current
-      if(videoEl){
+      if (videoEl) {
         videoEl.srcObject = stream
       }
     })
 
-  },[])
+  }, [])
 
-//Carregando os modelos
-  useEffect(() =>{
+  //Carregando os modelos
+  useEffect(() => {
     Promise.all([
       faceapi.loadTinyFaceDetectorModel('/models'),
       faceapi.loadFaceLandmarkModel('/models'),
       faceapi.loadFaceExpressionModel('/models'),
-    ]).then(() =>{
+    ]).then(() => {
       console.log('Models Loaded')
     })
   }, [])
 
-//Detectando a face
+  //Detectando a face
+  //*Colocar em um set interval*/
   useEffect(() => {
     const videoEl = videoRef.current
-    if(!videoEl) return
+    const canvasEl = canvasRef.current
+
+    if (!videoEl || !canvasEl) return
 
     async function detect() {
       const detection = await faceapi.detectSingleFace(
         videoEl, new faceapi.TinyFaceDetectorOptions()
-        )
+      )
+
       console.log(detection)
+
+      //Desenhar no canvas
+        if(detection){
+          const dimensions = {
+            width: videoEl.offsetWidth,
+            height: videoEl.offsetHeight,
+          }
+
+          faceapi.matchDimensions(canvasEl, dimensions)
+          const resizeResults = faceapi.resizeResults(detection, dimensions)
+          faceapi.draw.drawDetections(canvasEl, resizeResults)
+        }
     }
 
     detect()
   }, [])
+ //*Colocar em um set interval*/
+
 
 
   return (
     <section className="bg-gray-950 h-screen flex items-center justify-center">
 
       <div className="relative bg-slate-500 aspect-video">
-        <video autoPlay ref={videoRef}></video>
+        <div className="relative">
+          <video autoPlay ref={videoRef}></video>
+          <canvas ref={canvasRef} className="absolute inset-0 w-full h-full"></canvas>
+        </div>
       </div>
 
       <div className="w-1/2">
